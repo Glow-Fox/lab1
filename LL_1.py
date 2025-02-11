a9dab6a8240fa061323d8c187886261a5a5d0a20
Vladlens Medvedevs 241RDB004 <vladlens.medvedevs@edu.rtu.lv>
2025-02-11 07:28:59 +0000

    finished

--- LL_1.py
+++ LL_1.py
@@ -23,19 +23,41 @@ class LinkedList:
         
 
     # WRITE FIND_MIDDLE_NODE METHOD HERE #
-    #                                    #
-    #                                    #
-    #                                    #
-    #                                    #
+    def find_middle_node(self):
+        if self.head is None:
+            return None
+
+        slow = self.head
+        fast = self.head
+
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
     ######################################
 
 
 
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.append(5)
+def get_linked_list_from_input():
+    while True:
+        try:
+            values = list(map(int, input("Ievadiet skaitļus ar atstarpi: ").split()))
+            if not values:
+                raise ValueError("Saraksts nedrīkst būt tukšs")
+            break
+        except ValueError:
+            print("Kļūda! Ievadiet tikai veselus skaitļus.")
+    
+    linked_list = LinkedList(values[0])
+    for value in values[1:]:
+        linked_list.append(value)
+    
+    return linked_list
 
-print( my_linked_list.find_middle_node().value )
 
+my_linked_list = get_linked_list_from_input()
+
+middle_node = my_linked_list.find_middle_node()
+if middle_node:
+    print("Vidējais elements:", middle_node.value)
